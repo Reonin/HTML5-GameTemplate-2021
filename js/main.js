ee52409127ecc2098cc08b4fccbbe1f42a49dfcb
Corey J <coreyajeffers@gmail.com>
2021-01-25 03:09:39 -0500

    Missile module

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 import { scaletosmallest } from './scaletosmallest.js';
 import { Bullet } from './Bullet.js';
+import { Missile } from './Missile.js';
 import { Enemy } from './Enemy.js';
 import { collides } from './collisionBox.js';
 import { setUpKeys } from './setUpKeys.js';
 import { mapArray } from './mapArray.js';
 // import { collisionDetection } from './collisionDetection.js';
 
-
 scaletosmallest(canvasid, ratio);
 $(window).on('resize', () => {
-    scaletosmallest(canvasid, ratio);
+  scaletosmallest(canvasid, ratio);
 });
-
 
 // Draw tile map
 drawMap(canvas);
@@ -53,155 +52,150 @@ function getMousePos(canvas, evt) {
   };
 }
 
-  function drawMap(canvas) {
-    var background = canvas;
-    for (let i = 0; i < mapArray.length; i++) {
-      for (let j = 0; j < mapArray[i].length; j++) {
-        if (parseInt(mapArray[i][j]) == 0) {
-          var newTile = {
-              // color: "#00A",
-              tile: Tile('emptyTile'),
-              x: j * 60,
-              y: i * 60,
-              width:	60,
-              height: 60,
-              type: 'floor',
-              draw() {
-              // canvas.fillStyle = this.color;
-              // canvas.fillRect(this.x, this.y, this.width, this.height);
-              this.tile.draw(canvas, this.x, this.y, 60, 60);
-           },
-         };
-  
-          tileArray.push(newTile);
-          // console.log(newTile.x);
-          //  <!--canvas.drawImage(image1, x*28, y*28);-->
-          }
-        if (parseInt(mapArray[i][j]) == 1) {
-              var newTile = {
-              // color: "#00A",
-              tile: Tile('redTile'),
-              x: j * 60,
-              y: i * 60,
-              width: 60,
-              height: 60,
-            type: 'wall',
-  
-              draw() {
-              // canvas.fillStyle = this.color;
-              // canvas.fillRect(this.x, this.y, this.width, this.height);
-              this.tile.draw(canvas, this.x, this.y, 60, 60);
-            },
-          };
-          tileArray.push(newTile);
-          // canvas.drawImage(image2, x*28, y*28);-->
-        }
-        if (parseInt(mapArray[i][j]) == 3) {
-              var newTile = {
-              // color: "#00A",
-              tile: Tile('emptyTile'),
-              x: j * 60,
-              y: i * 60,
-              width: 60,
-              height: 60,
-            type: 'player1spawn',
-  
-              draw() {
-              // canvas.fillStyle = this.color;
-              // canvas.fillRect(this.x, this.y, this.width, this.height);
-              this.tile.draw(canvas, this.x, this.y, 60, 60);
-            },
-          };
-          tileArray.push(newTile);
-          player1spawns.push(newTile);
-          // canvas.drawImage(image2, x*28, y*28);-->
-        }
-        if (parseInt(mapArray[i][j]) == 2) {
-              var newTile = {
-              // color: "#00A",
-              tile: Tile('emptyTile'),
-              x: j * 60,
-              y: i * 60,
-              width: 60,
-              height: 60,
-            type: 'player2spawn',
-  
-              draw() {
-              // canvas.fillStyle = this.color;
-              // canvas.fillRect(this.x, this.y, this.width, this.height);
-              this.tile.draw(canvas, this.x, this.y, 60, 60);
-            },
-          };
-          tileArray.push(newTile);
-          player2spawns.push(newTile);
-          // canvas.drawImage(image2, x*28, y*28);-->
-        }
-  
-        // background.addChild(tile);-->
+function drawMap(canvas) {
+  const background = canvas;
+  for (let i = 0; i < mapArray.length; i++) {
+    for (let j = 0; j < mapArray[i].length; j++) {
+      if (parseInt(mapArray[i][j]) == 0) {
+        var newTile = {
+          // color: "#00A",
+          tile: Tile('emptyTile'),
+          x: j * 60,
+          y: i * 60,
+          width:	60,
+          height: 60,
+          type: 'floor',
+          draw() {
+            // canvas.fillStyle = this.color;
+            // canvas.fillRect(this.x, this.y, this.width, this.height);
+            this.tile.draw(canvas, this.x, this.y, 60, 60);
+          },
+        };
+
+        tileArray.push(newTile);
+        // console.log(newTile.x);
+        //  <!--canvas.drawImage(image1, x*28, y*28);-->
       }
+      if (parseInt(mapArray[i][j]) == 1) {
+        var newTile = {
+          // color: "#00A",
+          tile: Tile('redTile'),
+          x: j * 60,
+          y: i * 60,
+          width: 60,
+          height: 60,
+          type: 'wall',
+
+          draw() {
+            // canvas.fillStyle = this.color;
+            // canvas.fillRect(this.x, this.y, this.width, this.height);
+            this.tile.draw(canvas, this.x, this.y, 60, 60);
+          },
+        };
+        tileArray.push(newTile);
+        // canvas.drawImage(image2, x*28, y*28);-->
+      }
+      if (parseInt(mapArray[i][j]) == 3) {
+        var newTile = {
+          // color: "#00A",
+          tile: Tile('emptyTile'),
+          x: j * 60,
+          y: i * 60,
+          width: 60,
+          height: 60,
+          type: 'player1spawn',
+
+          draw() {
+            // canvas.fillStyle = this.color;
+            // canvas.fillRect(this.x, this.y, this.width, this.height);
+            this.tile.draw(canvas, this.x, this.y, 60, 60);
+          },
+        };
+        tileArray.push(newTile);
+        player1spawns.push(newTile);
+        // canvas.drawImage(image2, x*28, y*28);-->
+      }
+      if (parseInt(mapArray[i][j]) == 2) {
+        var newTile = {
+          // color: "#00A",
+          tile: Tile('emptyTile'),
+          x: j * 60,
+          y: i * 60,
+          width: 60,
+          height: 60,
+          type: 'player2spawn',
+
+          draw() {
+            // canvas.fillStyle = this.color;
+            // canvas.fillRect(this.x, this.y, this.width, this.height);
+            this.tile.draw(canvas, this.x, this.y, 60, 60);
+          },
+        };
+        tileArray.push(newTile);
+        player2spawns.push(newTile);
+        // canvas.drawImage(image2, x*28, y*28);-->
+      }
+
+      // background.addChild(tile);-->
     }
-    // canvas.addChild(background);-->
-    return tileArray;
   }
-  
+  // canvas.addChild(background);-->
+  return tileArray;
+}
 
 // Create The player
 window.player = {
-    // color: "#00A",
-    sprite: Sprite('spaceship'),
-    x: 220,
-    y: 680,
-    width: 32,
-    height: 32,
-    life: 100,
-    velX: 0,
-    velY: 0,
-    speed: 4,
-    friction: 0.85,
-    draw() {
-      // canvas.fillStyle = this.color;
-      // canvas.fillRect(this.x, this.y, this.width, this.height);
-      this.sprite.draw(canvas, this.x, this.y);
-    },
-    shoot() {
-      const bulletPosition = this.midpoint();
-      shoot_sound.play();
-  
-      window.playerBullets.push(new Bullet(5, bulletPosition.x, bulletPosition.y));
-    },
-    launch() {
-      const missilePostition = this.midpoint();
-      console.log(Missle.width);
-      window.playerMissiles.push(Missle({
-        speed: 2,
-        x: missilePostition.x - 500,
-        y: missilePostition.y,
-      }));
-    },
-    midpoint() {
-      return {
-        x: this.x + this.width / 2,
-        y: this.y + this.height / 2,
-      };
-    },
-    explode() {
-      this.active = false;
-      explosion_sound.play();
-      GameLoopMusic_sound.fadeOut(0, 2000);
-      window.currentState = window.states.End;
-      // An explosion sound and then end the game
-    },
-    lifeChange(change) {
-      this.life += change; // Adds or subtracts health based on the value added in the function
-  
-      if (this.life <= 0) {
-        this.explode();
-      }
-  
-      return this.life;
-    },
-  };
-  window.playerBullets = [];
+  // color: "#00A",
+  sprite: Sprite('spaceship'),
+  x: 220,
+  y: 680,
+  width: 32,
+  height: 32,
+  life: 100,
+  velX: 0,
+  velY: 0,
+  speed: 4,
+  friction: 0.85,
+  draw() {
+    // canvas.fillStyle = this.color;
+    // canvas.fillRect(this.x, this.y, this.width, this.height);
+    this.sprite.draw(canvas, this.x, this.y);
+  },
+  shoot() {
+    const bulletPosition = this.midpoint();
+    shoot_sound.play();
+
+    window.playerBullets.push(new Bullet(5, bulletPosition.x, bulletPosition.y));
+  },
+  launch() {
+    const missilePostition = this.midpoint();
+    console.log(Missile.width);
+    window.playerMissiles.push(new Missile(2, missilePostition.x - 500, missilePostition.y));
+  },
+  midpoint() {
+    return {
+      x: this.x + this.width / 2,
+      y: this.y + this.height / 2,
+    };
+  },
+  explode() {
+    this.active = false;
+    explosion_sound.play();
+    GameLoopMusic_sound.fadeOut(0, 2000);
+    window.currentState = window.states.End;
+    // An explosion sound and then end the game
+  },
+  lifeChange(change) {
+    this.life += change; // Adds or subtracts health based on the value added in the function
+
+    if (this.life <= 0) {
+      this.explode();
+    }
+
+    return this.life;
+  },
+};
+window.playerBullets = [];
 
 const context = canvas;
 
@@ -211,104 +205,101 @@ const context = canvas;
 //   writeMessage(canvas, message);
 // }, false);
 
-
 function update() { // Updates location and reaction of objects to the canvas
-    if (window.currentState === window.states.splash) {
-      // splashTextX += 1;
-      splashTextY += 1;
-  
-      if (splashTextY >= 300) {
-        window.currentState = window.states.title;
-      }
-    }
-  
-    if (window.currentState === window.states.title) {
-      if (keydown.space) {
-        window.currentState = window.states.Game;
-      }
-    }
-  
-    if (window.currentState === window.states.Game) {
-      // window.player Movement Controls
-      if (keydown.left) {
-        if (window.player.velX > -window.player.speed) {
-          window.player.velX--;
-        }
-      }
-  
-      if (keydown.right) {
-        if (window.player.velX < window.player.speed) {
-          window.player.velX++;
-        }
-      }
-  
-      if (keydown.up) {
-        if (window.player.velY > -window.player.speed) {
-          window.player.velY--;
-        }
-      }
-  
-      if (keydown.down) {
-        if (window.player.velY < window.player.speed) {
-          window.player.velY++;
-        }
-      }
-  
-      window.player.velX *= window.player.friction;
-      window.player.x += window.player.velX;
-      window.player.velY *= window.player.friction;
-      window.player.y += window.player.velY;
-      // prevents character from going past canvas
-      window.player.x = window.player.x.clamp(0, CANVAS_WIDTH - window.player.width);
-      // prevents character from going past canvas
-      window.player.y = window.player.y.clamp(0, CANVAS_HEIGHT - window.player.height);
-  
-      // window.player actions
-      if (keydown.space) {
-        window.player.shoot();
-      }
-  
-      window.playerBullets.forEach((bullet) => {
-        bullet.update();
-      });
-  
-      window.playerBullets = window.playerBullets.filter((bullet) => bullet.active);
-  
-      if (keydown.v) {
-        window.player.launch();
-      }
-  
-      playerMissiles.forEach((missle) => {
-        missle.update();
-      });
-  
-      playerMissiles = playerMissiles.filter((missle) => missle.active);
-  
-      // Enemy Update logic
-      window.enemies.forEach((enemy) => {
-        enemy.update();
-      });
-  
-      window.enemies = window.enemies.filter((enemy) => enemy.active);
-  
-      if (Math.random() < 0.1) {
-        window.enemies.push( new Enemy() );
-      }
-  
-      // Handle Collision
-      handleCollisions();
-    }
-  
-    if (window.currentState === window.states.End) {
-      endTextY += 1;
-  
-      if (endTextY >= 300) {
-        endTextY = 300;
-      }
+  if (window.currentState === window.states.splash) {
+    // splashTextX += 1;
+    splashTextY += 1;
+
+    if (splashTextY >= 300) {
+      window.currentState = window.states.title;
     }
   }
-  
 
+  if (window.currentState === window.states.title) {
+    if (keydown.space) {
+      window.currentState = window.states.Game;
+    }
+  }
+
+  if (window.currentState === window.states.Game) {
+    // window.player Movement Controls
+    if (keydown.left) {
+      if (window.player.velX > -window.player.speed) {
+        window.player.velX--;
+      }
+    }
+
+    if (keydown.right) {
+      if (window.player.velX < window.player.speed) {
+        window.player.velX++;
+      }
+    }
+
+    if (keydown.up) {
+      if (window.player.velY > -window.player.speed) {
+        window.player.velY--;
+      }
+    }
+
+    if (keydown.down) {
+      if (window.player.velY < window.player.speed) {
+        window.player.velY++;
+      }
+    }
+
+    window.player.velX *= window.player.friction;
+    window.player.x += window.player.velX;
+    window.player.velY *= window.player.friction;
+    window.player.y += window.player.velY;
+    // prevents character from going past canvas
+    window.player.x = window.player.x.clamp(0, CANVAS_WIDTH - window.player.width);
+    // prevents character from going past canvas
+    window.player.y = window.player.y.clamp(0, CANVAS_HEIGHT - window.player.height);
+
+    // window.player actions
+    if (keydown.space) {
+      window.player.shoot();
+    }
+
+    window.playerBullets.forEach((bullet) => {
+      bullet.update();
+    });
+
+    window.playerBullets = window.playerBullets.filter((bullet) => bullet.active);
+
+    if (keydown.v) {
+      window.player.launch();
+    }
+
+    playerMissiles.forEach((Missile) => {
+      Missile.update();
+    });
+
+    playerMissiles = playerMissiles.filter((Missile) => Missile.active);
+
+    // Enemy Update logic
+    window.enemies.forEach((enemy) => {
+      enemy.update();
+    });
+
+    window.enemies = window.enemies.filter((enemy) => enemy.active);
+
+    if (Math.random() < 0.1) {
+      window.enemies.push(new Enemy());
+    }
+
+    // Handle Collision
+    handleCollisions();
+  }
+
+  if (window.currentState === window.states.End) {
+    endTextY += 1;
+
+    if (endTextY >= 300) {
+      endTextY = 300;
+    }
+  }
+}
 
 function gameloop() {
   controller();
@@ -322,7 +313,6 @@ function gameloop() {
 var paused = false;
 
 window.requestAnimFrame(gameloop);
-
 
 setUpKeys();
 
@@ -354,48 +344,10 @@ const splashTextY = 0;
 const endTextX = CANVAS_WIDTH / 3;
 const endTextY = 0;
 
-
-
-
-
-
-
-
 window.playerMissiles = [];
 
-function Missle(I) {
-  I.active = true;
-
-  I.xVelocity = 0;
-  I.mousePosX = mousePos.x;
-  I.mousePosY = mousePos.y;
-  I.yVelocity = -I.speed;
-  I.width = 1002;
-  I.height = 32;
-  I.color = '#34DDDD';
-
-  I.inBounds = function () {
-    return I.x >= 0 && I.x <= CANVAS_WIDTH
-               && I.y >= 0 && I.y <= CANVAS_HEIGHT;
-  };
-
-  I.draw = function () {
-    canvas.fillStyle = this.color;
-    canvas.fillRect(this.x, this.y, this.width, this.height);
-  };
-
-  I.update = function () {
-    I.x += I.mousePosX;
-    I.y += I.mousePosY + I.yVelocity;
-
-    I.active = I.active && I.inBounds();
-  };
-
-  return I;
-}
 
 window.enemies = [];
-
 
 function handleCollisions() {
   window.playerBullets.forEach((bullet) => {
@@ -407,11 +359,11 @@ function handleCollisions() {
     });
   });
 
-  window.playerMissiles.forEach((missle) => {
+  window.playerMissiles.forEach((Missile) => {
     window.enemies.forEach((enemy) => {
-      if (collides(missle, enemy)) {
+      if (collides(Missile, enemy)) {
         enemy.explode();
-        missle.active = false;
+        Missile.active = false;
       }
     });
   });
@@ -423,8 +375,6 @@ function handleCollisions() {
     }
   });
 }
-
-
 
 function controller() {
   // Pause the game
