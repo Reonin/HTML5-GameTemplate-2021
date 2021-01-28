148f011c6808e216ad9b2f485783f0e276c21b68
Blake Balick-Schreiber <blakebalick-schreiber@Blakes-MacBook-Pro.local>
2021-01-27 23:00:07 -0500

    unfuckarooed

---
+++ api/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "127.0.0.1",
+    USER: "thefullpalette",
+    PASSWORD: "Pc5!e2J^Y7H7MY@i",
+    DB: "thefullpalette",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
