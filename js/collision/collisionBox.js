eefe6ac55e1cbf2d1b99e62c854e76758a32928a
Blake Balick-Schreiber <blakebalick-schreiber@Blakes-MacBook-Pro.local>
2021-01-27 22:11:40 -0500

    created a client folder, api folder, dockerfiles, websockets

--- js/collision/collisionBox.js
+++
@@ -1,7 +0,0 @@
-// Simple Collision Detection
-export function collides(a, b) {
-  return a.x < b.x + b.width
-             && a.x + a.width > b.x
-             && a.y < b.y + b.height
-             && a.y + a.height > b.y;
-}
--- js/utils/scaletosmallest.js
+++
@@ -1,9 +0,0 @@
-export default function scaletosmallest(canvasElement, ratio) {
-  if ( ($(window).width() / ratio) <= $(window).width() ) {
-    canvasElement.style.width = '100%';
-    canvasElement.style.height = 'auto';
-  } else {
-    canvasElement.style.height = '100%';
-    canvasElement.style.width = 'auto';
-  }
-}
