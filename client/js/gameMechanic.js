a539b2f4a107b03303b0772cde471506ec7546a8
Reonin <coreyajeffers@gmail.com>
2021-01-29 12:36:30 -0500

    Tagger has momentary speed boost and tagged cant move

--- client/js/gameMechanic.js
+++ client/js/gameMechanic.js
@@ -7,12 +7,14 @@ export default function transferTag(player, otherPlayer) {
     otherPlayer.isIt = true;
     player.score(100);
     thingsForBoth(player,otherPlayer);
+    changePlayerSpeed(player,otherPlayer);
   } else if (otherPlayer.isIt === true && player.isImmune === false) {
     console.log('P2 Tagged P1');
     otherPlayer.isIt = false;
     player.isIt = true;
     otherPlayer.score(100);
     thingsForBoth(player,otherPlayer);
+    changePlayerSpeed(otherPlayer,player);
   }
 }
 
@@ -27,4 +29,16 @@ function removeImmunityforAll(){
   window.playerArray.forEach(p => {
     p.isImmune = false;
   });
+}
+
+function changePlayerSpeed(tagger, tagged){
+  tagger.speed = 8;
+  tagged.speed = 0;
+  countdownTimer(restoreSpeed);
+}
+
+function restoreSpeed(){
+  window.playerArray.forEach(p => {
+    p.speed = 4;
+  })
 }
