b744be75269c943016f1d5f1ff35a8f324dcfd0e
Reonin <coreyajeffers@gmail.com>
2021-01-29 09:04:40 -0500

    Player Collision

--- client/js/collision/handleCollisions.js
+++ client/js/collision/handleCollisions.js
@@ -1,30 +1,36 @@
 import { collides } from './collisionBox.js';
 
 export default function handleCollisions() {
-  window.playerBullets.forEach((bullet) => {
-    window.enemies.forEach((enemy) => {
-      if (collides(bullet, enemy)) {
-        enemy.explode();
-        bullet.active = false;
-      }
-    });
-  });
+  // window.playerBullets.forEach((bullet) => {
+  //   window.enemies.forEach((enemy) => {
+  //     if (collides(bullet, enemy)) {
+  //       enemy.explode();
+  //       bullet.active = false;
+  //     }
+  //   });
+  // });
 
-  window.playerMissiles.forEach((Missile) => {
-    window.enemies.forEach((enemy) => {
-      if (collides(Missile, enemy)) {
-        enemy.explode();
-        Missile.active = false;
-      }
-    });
-  });
+  // window.playerMissiles.forEach((Missile) => {
+  //   window.enemies.forEach((enemy) => {
+  //     if (collides(Missile, enemy)) {
+  //       enemy.explode();
+  //       Missile.active = false;
+  //     }
+  //   });
+  // });
 
-  window.enemies.forEach((enemy) => {
-    if (collides(enemy, player)) {
-      enemy.explode();
-      player.lifeChange(-10);
-    }
-  });
+  // window.enemies.forEach((enemy) => {
+  //   if (collides(enemy, player)) {
+  //     enemy.explode();
+  //     player.lifeChange(-10);
+  //   }
+  // });
+  window.playerArray.forEach((player) => {
+    // if (collides(player, player)) {
+    //   console.log("TEST");
+    // }
+  })
+
 
   // Pickups Collision
   pickups.forEach((pickup) => {
