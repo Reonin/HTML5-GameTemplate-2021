cbeb904f3a0a8284f0023bfcb14c4c58fa11168c
Corey J <coreyajeffers@gmail.com>
2021-01-30 13:44:05 -0500

    Pushing player changes for trails

--- client/js/update.js
+++ client/js/update.js
@@ -60,7 +60,7 @@ export default async function update() { // Updates location and reaction of obj
       }
     }
       //fast pass to game
-      //window.currentState = window.states.GAME;
+      // window.currentState = window.states.GAME;
       if (window.allPlayersSet == true) {
        window.currentState = window.states.GAME;
        }
